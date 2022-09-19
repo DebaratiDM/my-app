025747f606f01bd3d918cc3a6282eb00ff1a7a69
Debarati Das <debarati84430@gmail.com>
2022-09-19 13:53:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Footer } from "./components/common/Footer";
+import { About } from "./pages/About";
+import { Header } from "./components/common/Header";
+import { Contact } from "./pages/Contact";
+import { Post } from "./pages/Post";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/post" element={<Post />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/common/Banner.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Banner = (props) => {
+  const { banner, title, desc } = props;
+  return (
+    <header className="masthead" style={{ backgroundImage: `url(${banner})` }}>
+      <div className="container position-relative px-4 px-lg-5">
+        <div className="row gx-4 gx-lg-5 justify-content-center">
+          <div className="col-md-10 col-lg-8 col-xl-7">
+            <div className="page-heading">
+              <h1>{title}</h1>
+              <span className="subheading">{desc}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
